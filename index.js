be41f5b48069494d3dbd4d8cea2f8953eaf058e8
G00berDev <adeptbee9@gmail.com>
2025-04-08 13:47:30 -0700

    TS PMO
    TS TS TS

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+var souls_borne_name = "ornstein";
+var souls_collected = 500;
+var chosen_undead = true;
+let greeting = "Arise";
---
+++ index.ts
@@ -0,0 +1,5 @@
+var souls_borne_name : string = "ornstein";
+var souls_collected : number = 500;
+var chosen_undead : boolean = true;
+
+let greeting = "Arise"
