30b42ac11e66be857f04eb31d78574ecca825df9
G00berDev <adeptbee9@gmail.com>
2025-04-15 14:00:03 -0700

    qawsed
    edfv

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 "use strict";
-var souls_borne_name = "ornstein";
-var souls_collected = 500;
-var chosen_undead = true;
+let souls_borne_name = "ornstein";
+let souls_collected = 500;
+let chosen_undead = true;
 let greeting = "Arise";
+function add(n1, n2) {
+    return n1 + n2;
+}
+console.log(add(150958406, 3457408670));
+const addArrow = (n1, n2) => n1 + n2;
+const printResult = (num) => {
+    console.log('Result: ' + num);
+};
+const person = {
+    name: "Ornstein",
+    age: 1000,
+    hobbies: ["lightning spears", "dragon hunting"],
+    role: [1, "Executioner"]
+};
+let product = {
+    id: "soul_of_smough",
+    price: 10000,
+    tags: ["rare", "boss", "forgeable"],
+    details: {
+        title: "Soul of Smough",
+        description: "The soul of Executioner Smough, used to forge powerful weapons."
+    }
+};
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role[Role["READ_ONLY"] = 0] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 100] = "AUTHOR";
+})(Role || (Role = {}));
+let answer;
+answer = "false";
+function greet(user) {
+    console.log("Hi, I am " + user.name);
+}
+function isOlder(user, checkAge) {
+    return checkAge > user.age;
+}
+console.log(add(10, 20)); // Outputs: 30
--- index.ts
+++ index.ts
@@ -1,5 +1,89 @@
-var souls_borne_name : string = "ornstein";
-var souls_collected : number = 500;
-var chosen_undead : boolean = true;
+let souls_borne_name: string = "ornstein";
+let souls_collected: number = 500;
+let chosen_undead: boolean = true;
 
-let greeting = "Arise"
+let greeting = "Arise";
+
+function add(n1: number, n2: number): number {
+    return n1 + n2;
+}
+
+console.log(add(150958406, 3457408670));
+
+const addArrow = (n1: number, n2: number): number => n1 + n2;
+
+const printResult = (num: number): void => {
+    console.log('Result: ' + num);
+};
+
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
+    name: "Ornstein",
+    age: 1000,
+    hobbies: ["lightning spears", "dragon hunting"],
+    role: [1, "Executioner"]
+};
+
+let product: {
+    id: string;
+    price: number;
+    tags: string[];
+    details: {
+        title: string;
+        description: string;
+    };
+} = {
+    id: "soul_of_smough",
+    price: 10000,
+    tags: ["rare", "boss", "forgeable"],
+    details: {
+        title: "Soul of Smough",
+        description: "The soul of Executioner Smough, used to forge powerful weapons."
+    }
+};
+
+enum Role {
+    ADMIN = "ADMIN",
+    READ_ONLY = 0,
+    AUTHOR = 100,
+}
+
+let answer: string | number | boolean;
+answer = "false";
+
+type Combinable = number | string;
+
+type User = {
+    name: string;
+    age: number;
+};
+
+function greet(user: User) {
+    console.log("Hi, I am " + user.name);
+}
+
+function isOlder(user: User, checkAge: number) {
+    return checkAge > user.age;
+}
+
+
+console.log(add(10, 20)); // Outputs: 30
+
+interface Character {
+    name: string;
+    class: "Knight" | "Sorcerer" | "Thief";
+    level: number;
+    stats: {
+        strength: number;
+        dexterity: number;
+        intelligence: number;
+        faith: number;
+    };
+    levelUp: () => void;
+}
+
+
